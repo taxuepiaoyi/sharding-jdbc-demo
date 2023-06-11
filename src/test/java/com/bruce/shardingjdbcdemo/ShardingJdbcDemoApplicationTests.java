0e3bbd89ba983b4f49878d4a326da865f602b482
上官镜文 <shangguanjingwen@zj.tech>
2023-06-11 13:41:47 +0800

    配置sharding-jdbc

--- src/test/java/com/bruce/shardingjdbcdemo/ShardingJdbcDemoApplicationTests.java
+++ src/test/java/com/bruce/shardingjdbcdemo/ShardingJdbcDemoApplicationTests.java
@@ -1,6 +1,8 @@
 package com.bruce.shardingjdbcdemo;
 
 import com.bruce.shardingjdbcdemo.dto.CourseDTO;
+import com.bruce.shardingjdbcdemo.entity.Course;
+import com.bruce.shardingjdbcdemo.mapper.CourseMapper;
 import com.bruce.shardingjdbcdemo.service.CourseService;
 import net.bytebuddy.implementation.bind.annotation.RuntimeType;
 import org.junit.jupiter.api.Test;
@@ -16,13 +18,26 @@ class ShardingJdbcDemoApplicationTests {
     @Autowired
     private CourseService courseService ;
 
+    @Autowired
+    private CourseMapper courseMapper ;
+
     @Test
-    void saveCourse(){
+    void saveCourseDTO(){
         CourseDTO courseDTO = CourseDTO.builder().build();;
         courseDTO.setCname("java");
         courseDTO.setUserId(100L);
         courseDTO.setCstatus("Normal");
         courseDTO.setCreateTime(LocalDateTime.now());
         courseService.saveCourse(courseDTO) ;
+    }
+
+    @Test
+    void saveCourse(){
+        Course course = Course.builder().build();;
+        course.setCname("java");
+        course.setUserId(100L);
+        course.setCstatus("Normal");
+        course.setCreateTime(LocalDateTime.now());
+        courseMapper.insert(course) ;
     }
 }
